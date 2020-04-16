a4a81092999be36bf8b68e8935ed8a816361972b
David Munro <dbhmunro@gmail.com>
2020-04-16 20:22:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,20 @@ def now_serving (katz_deli_line)
   katz_deli_line.shift
   puts (katz_deli_line.length == 0) ? "There is nobody waiting to be served!" : "Currently serving #{serving}."
 end
+
+def line (katz_deli_line)
+  if (katz_deli_line.length == 0)
+    puts "The line is currently empty."
+  else
+    line_announce = "The line is currently: 1. #{katz_deli_line[0]}"
+    n = 1
+    loop do
+      line_announce = "#{line_announce}, #{n + 1}. #{katz_deli_line[n]}"
+      n += 1
+      if n >= katz_deli_line.length
+        break
+      end
+      puts line_announce
+    end
+  end
+  
