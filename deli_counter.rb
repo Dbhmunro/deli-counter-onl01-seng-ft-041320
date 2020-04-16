72a640fba21048a2cef5c818a9cac6476ac21097
David Munro <dbhmunro@gmail.com>
2020-04-16 20:09:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,3 +2,9 @@ def take_a_number (katz_deli_line, name)
   katz_deli_line << name
   puts "Welcome, #{name}. You are number #{katz_deli_line.length} in line."
 end
+
+def now_serving
+  serving = katz_deli_line.first
+  katz_deli_line.shift
+  puts (katz_deli_line == 0) ? "There is nobody waiting to be served!" : "Currently serving #{serving}."
+end
