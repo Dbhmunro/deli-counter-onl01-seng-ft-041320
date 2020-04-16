5599b098920509354196e4aeac9001580517ea5c
David Munro <dbhmunro@gmail.com>
2020-04-16 20:24:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def line (katz_deli_line)
     loop do
       line_announce = "#{line_announce}, #{n + 1}. #{katz_deli_line[n]}"
       n += 1
-      if n >= katz_deli_line.length
+      if n >= katz_deli_line.length + 1
         break
       end
       puts line_announce
